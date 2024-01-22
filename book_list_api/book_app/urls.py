1378a96d31b502a6aa13304a49060d762a2ee1ea
Decodeine <abionaadesola@yahoo.com>
2024-01-22 23:22:44 +0100

    single_book.html

--- book_list_api/book_app/urls.py
+++ book_list_api/book_app/urls.py
@@ -4,7 +4,7 @@ from .views import ReviewListView #book_list_view
 
 urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
-    path('books/<int:pk>', views.SingleBookView.as_view()),
+    path('books/<int:pk>', views.SingleBookView.as_view(), name='singlebook'),
     path('books/<int:book_id>/reviews/', ReviewListView.as_view(), name='book_reviews'),
     #path('books/', book_list_view, name='books'),
 ]
