f9a7171fe1ed6f2f0a0afa760db6ae741f1ba704
Decodeine <abionaadesola@yahoo.com>
2024-01-22 15:07:20 +0100

    single_book.html

--- book_list_api/book_app/urls.py
+++ book_list_api/book_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import ReviewListView,book_list_view
+from .views import ReviewListView #book_list_view
 
 urlpatterns = [
-    #path('books', views.BookListView.as_view()),
+    path('books/', views.BookListView.as_view(), name='books'),
     path('books/<int:pk>', views.SingleBookView.as_view()),
     path('books/<int:book_id>/reviews/', ReviewListView.as_view(), name='book_reviews'),
-    path('books/', book_list_view, name='books'),
+    #path('books/', book_list_view, name='books'),
 ]
--- book_list_api/book_app/views.py
+++ book_list_api/book_app/views.py
@@ -3,20 +3,48 @@ from .serializers import BookSerializer, ReviewSerializer
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import render
+from django.http import JsonResponse,HttpResponseForbidden
+
+
 
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['title', 'author', 'publication_date']  
+    filterset_fields = ['title', 'author', 'publication_date']
 
-def book_list_view(request):
-    books = Book.objects.all()
-    return render(request, 'home.html', {'books': books})
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.filter_queryset(self.get_queryset())
+        return render(request, 'home.html', {'books': self.object_list})
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_staff:  # Check if the user is an admin
+            return HttpResponseForbidden("You are not allowed to create books.")
+        response = super().post(request, *args, **kwargs)
+        return JsonResponse(response.data)
+
+
+
 
 class SingleBookView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return render(request, 'single_book.html', {'book': self.object})
+
+    def put(self, request, *args, **kwargs):
+        response = super().put(request, *args, **kwargs)
+        return JsonResponse(response.data)
+
+    def patch(self, request, *args, **kwargs):
+        response = super().patch(request, *args, **kwargs)
+        return JsonResponse(response.data)
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        return JsonResponse(response.data)
 
 
 
